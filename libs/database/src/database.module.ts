d38feac6d4bc11fecfbf9142cbd1da2b5090c9eb
Tai Tien <superhirodev@gmail.com>
2025-04-28 09:02:03 +0700

    add

--- libs/database/src/database.module.ts
+++ libs/database/src/database.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { DatabaseService } from './database.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return {
+          uri: configService.get('MONGOOSE_URL'),
+        };
+      },
+    }),
+  ],
   providers: [DatabaseService],
   exports: [DatabaseService],
 })
